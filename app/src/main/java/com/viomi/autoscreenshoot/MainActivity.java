f612aaec91d6f688c2a70730778d22b530773b21
yhm2046 <yhm2046@gmail.com>
2021-03-30 15:44:22 +0800

    开启线程动态显示中间时间

--- app/src/main/java/com/viomi/autoscreenshoot/MainActivity.java
+++ app/src/main/java/com/viomi/autoscreenshoot/MainActivity.java
@@ -1,10 +1,13 @@
 package com.viomi.autoscreenshoot;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.ActivityInfo;
 import android.graphics.Point;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.text.format.DateFormat;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -19,8 +22,9 @@ import android.widget.TextView;
  * 2021.3.16    主界面模拟粤康码显示
  */
 public class MainActivity extends AppCompatActivity {
-    private TextView tvTime1,tvTime2,tvTime3;
-    private String TAG="wp-MainActivity";
+    private TextView tvTime1, tvTime2, tvTime3;
+    private String TAG = "wp-MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,64 +38,51 @@ public class MainActivity extends AppCompatActivity {
         initData();
     }
 
+    private Handler mHandler=new Handler(){ //处理时间显示实时刷新
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what){
+                case 1:
+                    long sysTime=System.currentTimeMillis();
+                    CharSequence sysTimeStr= DateFormat.format("yyyy-MM-dd HH:mm:ss", sysTime);
+                    Log.i(TAG,"xwg sysTimeStr:"+sysTimeStr);
+                    tvTime1.setText(DateFormat.format("HH:mm", sysTime));    //右上角时间
+                    tvTime2.setText(sysTimeStr);    //中间时间
+                    break;
+                default:
+                    break;
+            }//end switch
+        }   //end handleMessage
+    };//end Handler
+    /**
+     * 线程每秒计数
+     */
+    class TimeThread extends Thread{
+        public void run(){
+            do{
+                try{
+                    Thread.sleep(1000);
+                    Message msg=new Message();
+                    msg.what=1;
+                    mHandler.sendMessage(msg);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                    Log.i(TAG,"TimeThread error:"+e);
+                }
+            }while (true);
+        }//end run()
+    }//end TimeThread
+
     private void initData() {
-        long sysTime=System.currentTimeMillis();
-        CharSequence sysTimeStr= DateFormat.format("yyyy-MM-dd HH:mm:ss",sysTime);
-        Log.i(TAG,"sysTimeStr:"+sysTimeStr);
-        tvTime1.setText(DateFormat.format("HH:mm",sysTime));    //右上角时间
-        tvTime2.setText(sysTimeStr);    //中间时间
-//        tvTime2.setGravity(Gravity.CENTER);
         tvTime3.setVisibility(View.GONE);   //隐藏
-        getScreen1();
-        getScreen2();
-        getScreen3();
-        getScreen4();
+        new TimeThread().start();
     }
 
     private void initView() {
-        tvTime1=(TextView)findViewById(R.id.tvTime1);
-        tvTime2=(TextView)findViewById(R.id.tvTime2);
-        tvTime3=(TextView)findViewById(R.id.tvTime3);
+        tvTime1 = (TextView) findViewById(R.id.tvTime1);
+        tvTime2 = (TextView) findViewById(R.id.tvTime2);
+        tvTime3 = (TextView) findViewById(R.id.tvTime3);
 
-    }
-    public void getScreen1(){   //实际分辨率1
-        WindowManager windowManager = getWindow().getWindowManager();
-        Point point = new Point();
-        windowManager.getDefaultDisplay().getRealSize(point);
-//屏幕实际宽度（像素个数）
-        int width = point.x;
-//屏幕实际高度（像素个数）
-        int height = point.y;
-        Log.i(TAG,"screen1:"+width+"*"+height);
-    }
-    public void getScreen2(){   //实际分辨率2
-        WindowManager windowManager = getWindow().getWindowManager();
-        DisplayMetrics metrics = new DisplayMetrics();
-        windowManager.getDefaultDisplay().getRealMetrics(metrics);
-//屏幕实际宽度（像素个数）
-        int width = metrics.widthPixels;
-//屏幕实际高度（像素个数）
-        int height = metrics.heightPixels;
-        Log.i(TAG,"screen2:"+width+"*"+height);
-    }
-    public void getScreen3(){   //可用分辨率1
-        WindowManager windowManager = getWindow().getWindowManager();
-        Display display = windowManager.getDefaultDisplay();
-        Point point = new Point();
-        display.getSize(point);
-//屏幕可用宽度(像素个数)
-        int width = point.x;
-//屏幕可用高度(像素个数)
-        int height = point.y;
-        Log.i(TAG,"screen3:"+width+"*"+height);
-    }
-    public void getScreen4(){   //可用分辨率2
-        WindowManager windowManager = getWindow().getWindowManager();
-        Display display = windowManager.getDefaultDisplay();
-//屏幕可用宽度(像素个数)
-        int width = display.getWidth();
-//屏幕可用高度(像素个数)
-        int height = display.getHeight();
-        Log.i(TAG,"screen4:"+width+"*"+height);
     }
 }
